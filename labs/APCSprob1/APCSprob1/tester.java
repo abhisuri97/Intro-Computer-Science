c2ef1b63e7cbe92fae77d9bf9035ee42f0355cce
webfreak7 <suriabhinav@hotmail.com>
2015-01-16 13:43:34 -0600

    little changes here and there

--- labs/APCSprob1/APCSprob1/tester.java
+++ labs/APCSprob1/APCSprob1/tester.java
@@ -9,15 +9,11 @@ public class tester
 {
     public static String tester (String word)
     {
-        for(int i = 0 ; i < word.length(); i++)
+        for(int i = 1; i < word.length(); i++)
         {
-            if (word.substring(i,i+1).equals("A") && !word.substring(i+1,i+2).equals("A"))
+            if("A".equals(word.substring(i-1, i)) && !"A".equals(word.substring(i, i+1)))
             {
-                String str1 = word.substring (0,i);
-                String after = word.substring (i+1, i+2);
-                String A = "A";
-                String last = word.substring (i+2, word.length());
-                word = str1 + after + A + last;
+                word = word.substring(0, i-1) + word.substring(i, i+1) + word.substring(i-1, i) + word.substring(i+1);
                 i++;
             }
         }
