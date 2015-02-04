513a5ab564a5dedd601b5f2274a8d4557ca96374
webfreak7 <suriabhinav@hotmail.com>
2015-02-04 15:24:10 -0600

    Deck tester

--- labs/Elevens 2015/Activity2  Starter Code/DeckTester.java
+++ labs/Elevens 2015/Activity2  Starter Code/DeckTester.java
@@ -11,5 +11,10 @@ public class DeckTester {
 		String [] rank = new String []{"Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"};
 		String [] suits = new String [] {"Hearts", "Diamonds", "Clubs", "Spades"};
 		int [] points = new int [] {11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		Deck deck1 = new Deck(rank,suits,points);
+		System.out.println("Is deck1 empty? " + deck1.isEmpty());
+		System.out.println("The size of the deck1 is " + deck1.size());
+		System.out.println("Deal card from deck1 = " + deck1.deal());
+		System.out.println("All the cards from deck1 " + deck1.toString());
 	}
 }
