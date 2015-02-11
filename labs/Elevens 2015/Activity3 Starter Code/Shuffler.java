1677d6396f15d5c3e2de59d8f4e63a3cde33f1d1
webfreak7 <suriabhinav@hotmail.com>
2015-02-11 15:24:57 -0600

    fixed activities

--- labs/Elevens 2015/Activity3 Starter Code/Shuffler.java
+++ labs/Elevens 2015/Activity3 Starter Code/Shuffler.java
@@ -8,12 +8,12 @@ public class Shuffler {
 	 * The number of consecutive shuffle steps to be performed in each call
 	 * to each sorting procedure.
 	 */
-	private static final int SHUFFLE_COUNT = 1;
+	private static final int SHUFFLE_COUNT = 5;
 
 	/**
 	 * The number of values to shuffle.
 	 */
-	private static final int VALUE_COUNT = 4;
+	private static final int VALUE_COUNT = 5;
 
 	/**
 	 * Tests shuffling methods.
